dd586a97667321b23ffe9aa40ce4086d67b221df
jHelpinstill <jakob.helpinstill@gmail.com>
2023-09-13 18:30:59 -0500

    Add Widget class

---
+++ Widget.cpp
@@ -0,0 +1,115 @@
+#include "Widget.h"
+
+Widget::Widget(int x, int y, int width, int height)
+{
+	setPos(x, y);
+	setSize(width, height);
+
+	regions.push_back(new Region(Rect(0, 0, width, height, Color::LIGHT_GRAY, Color::DARK_GRAY), "default"));
+}
+
+Widget::Widget(
+	int x, int y, int width, int height,
+	std::initializer_list<Button*> buttons,
+	std::initializer_list<Textbox*> textboxes,
+	std::initializer_list<Region*> regions)
+{
+	setPos(x, y);
+	setSize(width, height);
+
+	for (Button* b : buttons)
+		this->buttons.push_back(b);
+
+	for (Textbox* t : textboxes)
+		this->textboxes.push_back(t);
+
+	for (Region* r : regions)
+		this->regions.push_back(r);
+}
+
+void Widget::setPos(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+void Widget::setSize(int width, int height)
+{
+	delete frame.pixels;
+	frame.pixels = new uint32_t[width * height];
+	frame.width = width;
+	frame.height = height;
+	body = Canvas(&frame);
+}
+
+void Widget::draw(Canvas& canvas, MouseInfo& mouse)
+{
+	for (Button* button : buttons)
+		button->checkMouse(mouse, x, y);
+
+	drawSelf();
+
+	canvas.drawMatrix(x, y, frame.width, frame.height, (Color*)frame.pixels);
+}
+
+void Widget::drawSelf()
+{
+	for (Region* region : regions)
+		body.drawRect(region->body);
+
+	for (Button* button : buttons)
+		button->draw(body);
+
+	for (Textbox* text : textboxes)
+		text->draw(body);
+}
+
+Button& Widget::button(std::string name)
+{
+	static Button dummy;
+	for (Button* button : buttons)
+	{
+		if (button->name.compare(name) == 0)
+		{
+			return *button;
+		}
+	}
+	return dummy;
+}
+Textbox& Widget::textbox(std::string name)
+{
+	static Textbox dummy;
+	for (Textbox* text : textboxes)
+	{
+		if (text->name.compare(name) == 0)
+		{
+			return *text;
+		}
+	}
+	return dummy;
+}
+Region& Widget::region(std::string name)
+{
+	static Region dummy;
+	for (Region* region : regions)
+	{
+		if (region->name.compare(name) == 0)
+		{
+			return *region;
+		}
+	}
+	return dummy;
+}
+
+Widget::~Widget()
+{
+	delete frame.pixels;
+	for (Region* region : regions)
+		delete region;
+
+	for (Button* button : buttons)
+		delete button;
+
+	for (Textbox* text : textboxes)
+		delete text;
+}
---
+++ Widget.h
@@ -0,0 +1,65 @@
+#pragma once
+#ifndef WIDGET_
+#define WIDGET_
+
+#include "Canvas.h"
+#include "MouseInfo.h"
+#include "Textbox.h"
+#include "Color.h"
+#include "Button.h"
+#include "Rect.h"
+
+#include <vector>
+#include <string>
+#include <initializer_list>
+
+struct Region
+{
+	Rect body;
+	std::string name;
+	Region() {}
+	Region(Rect body, std::string name)
+	{
+		this->body = body;
+		this->name = name;
+	}
+};
+
+class Widget
+{
+private:
+
+	int x;
+	int y;
+
+	Frame frame;
+	Canvas body;
+
+	std::vector<Button*> buttons;
+	std::vector<Textbox*> textboxes;
+	std::vector<Region*> regions;
+
+	void drawSelf();
+
+public:
+
+	Widget() {}
+	Widget(int x, int y, int width, int height);
+	Widget(
+		int x, int y, int width, int height,
+		std::initializer_list<Button*> buttons,
+		std::initializer_list<Textbox*> textboxes,
+		std::initializer_list<Region*> regions);
+
+	void setPos(int x, int y);
+	void setSize(int width, int height);
+	void draw(Canvas& canvas, MouseInfo& mouse);
+
+	Button& button(std::string name);
+	Textbox& textbox(std::string name);
+	Region& region(std::string name);
+
+	~Widget();
+};
+
+#endif
